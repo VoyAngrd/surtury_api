092af15f07da29c43d2fe86a3bbd458ea9acace4
Eduardo Mariano de Oliveira <eduardo.oliveira0366@gmail.com>
2022-10-02 23:02:02 +0000

    FUtility: feat - add function 'isObjectEmpty()'

--- api/functions/utilityFunctions.js
+++ api/functions/utilityFunctions.js
@@ -1,1 +1,13 @@
-module.exports = {}
+function isObjectEmpty(object) {
+    for (var property in object) {
+        if (Object.prototype.hasOwnProperty.call(object, property)) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+module.exports = {
+    isObjectEmpty
+}
