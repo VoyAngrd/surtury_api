ddc7b8f62884a669d054d22738d421f20fec0ff8
Eduardo Mariano de Oliveira <eduardo.oliveira0366@gmail.com>
2022-10-03 00:20:00 +0000

    FYouTube: feat - add function 'getChannelVideos()'

--- api/functions/youtubeFunctions.js
+++ api/functions/youtubeFunctions.js
@@ -1,1 +1,21 @@
-module.exports = {}
+const usetube = require('usetube')
+const { isObjectEmpty } = require('./utilityFunctions');
+
+const channelID = `UCcNST01YviJ0tMunSa0OwTg`
+var channelVideoList = {}
+
+async function getChannelVideos() {
+    let videoList = await usetube.getChannelVideos(channelID)
+
+    if (isObjectEmpty(videoList)) {
+        return false
+    }
+
+    channelVideoList = videoList
+    module.exports.channelVideoList = channelVideoList
+    return true
+}
+
+module.exports = {
+    getChannelVideos
+}
