1513f3bdf61cd77eba258bec86223299a1bca7b1
Eduardo Mariano de Oliveira <eduardo.oliveira0366@gmail.com>
2022-10-05 20:03:19 +0000

    FYouTube: feat - add route '/youtube/channel-video-list'

--- api/routes/youtubeRoute.js
+++ api/routes/youtubeRoute.js
@@ -1,5 +1,20 @@
 const youtube = require('../functions/youtubeFunctions')
+const { isObjectEmpty } = require('../functions/utilityFunctions')
 const { Router } = require('express')
 const routes = Router()
+
+youtube.start()
+
+routes.get('/youtube/channel-video-list', (req, res) => {
+    if (isObjectEmpty(youtube.channelVideoList)) {
+        return res.status(500).json({
+            "message": "Channel video list hasn't been created yet"
+        })
+    }
+
+    return res.status(200).json({
+        "message": youtube.channelVideoList
+    })
+})
 
 module.exports = routes
