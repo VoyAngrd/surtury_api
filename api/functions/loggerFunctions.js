02ae69490f76160ad0721b0b3b66021b799df7f1
Eduardo Mariano de Oliveira <eduardo.oliveira0366@gmail.com>
2022-10-09 19:38:56 +0000

    FLogger: feat - add function 'writeToConsole()'

--- api/functions/loggerFunctions.js
+++ api/functions/loggerFunctions.js
@@ -1,5 +1,19 @@
+const chalk = require('chalk')
+const { currentLocalDate } = require('./utilityFunctions')
 const config = require('../config/loggerConfig')
 
 function getLevelName(level) {
     return level && config.levels.hasOwnProperty(level) ? level : `info`
+}
+
+function writeToConsole(levelName, message, error = null) {
+    let level = config.levels[levelName]
+
+    let chalkFunction = chalk.hex(level.color)
+
+    message = error ? `${error.message} \n ${error.stack}` : message
+
+    let header = `[${levelName.toUpperCase()}] (${currentLocalDate()})`
+
+    console.log(`${chalkFunction(header)}: ${chalkFunction(message)}`)
 }
