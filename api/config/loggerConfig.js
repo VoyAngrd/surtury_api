a7c76d901bfcf3d4fa4adf7d509c0524dad9c899
Eduardo Mariano de Oliveira <eduardo.oliveira0366@gmail.com>
2022-10-09 16:25:19 +0000

    FLogger: feat - add config file 'loggerConfig.js'

---
+++ api/config/loggerConfig.js
@@ -0,0 +1,32 @@
+module.exports = {
+    levels: {
+        access: {
+            color: '#0052cc',
+            writeToFile: true
+        },
+        warn: {
+            color: '#ff991f',
+            writeToFile: true
+        },
+        debug: {
+            color: '#36b37e',
+            writeToFile: true
+        },
+        system: {
+            color: '#217b41',
+            writeToFile: true
+        },
+        info: {
+            color: '#6554c0',
+            writeToFile: true
+        },
+        error: {
+            color: '#ff3333',
+            writeToFile: true
+        },
+        fatal: {
+            color: '#e40001',
+            writeToFile: true
+        }
+    }
+}
